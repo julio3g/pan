843b6fb42f0cb58a7aabd40d8f9019845531b5bb
Julio Cesar Orso <juliocesar.orso@gmail.com>
2021-12-28 15:05:09 -0300

    add carousel

--- client/user/src/pages/index.tsx
+++ client/user/src/pages/index.tsx
@@ -135,30 +135,28 @@ export default function Home() {
           </li>
         </ul>
       </section>
-      <section>
-        <Carousel
-          swipeable={false}
-          draggable={false}
-          showDots={true}
-          responsive={responsive}
-          ssr={true} // means to render carousel on server-side.
-          infinite={true}
-          autoPlay={this.props.deviceType !== 'mobile' ? true : false}
-          autoPlaySpeed={1000}
-          keyBoardControl={true}
-          customTransition="all .5"
-          transitionDuration={500}
-          containerClass="carousel-container"
-          removeArrowOnDeviceType={['tablet', 'mobile']}
-          deviceType={this.props.deviceType}
-          dotListClass="custom-dot-list-style"
-          itemClass="carousel-item-padding-40-px"
-        >
-          <div>Item 1</div>
-          <div>Item 2</div>
-          <div>Item 3</div>
-          <div>Item 4</div>
-        </Carousel>
+      <section className={styles.slide}>
+        <div>
+          <Carousel
+            swipeable={false}
+            draggable={false}
+            showDots={false}
+            responsive={responsive}
+            ssr={true} // means to render carousel on server-side.
+            infinite={true}
+            arrows={true}
+            autoPlaySpeed={1000}
+            keyBoardControl={true}
+            customTransition="all .5"
+            transitionDuration={500}
+            removeArrowOnDeviceType={['tablet', 'mobile']}
+          >
+            <div>Item 1</div>
+            <div>Item 2</div>
+            <div>Item 3</div>
+            <div>Item 4</div>
+          </Carousel>
+        </div>
       </section>
     </main>
   );
